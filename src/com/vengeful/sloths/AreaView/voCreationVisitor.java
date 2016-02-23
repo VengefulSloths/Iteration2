732085e0154b56c64a6a09ed510cb1e6c15152a6
Alex Smith <alexs10@ufl.edu>
2016-02-23 14:40:59 -0500

    cleaned up the model visitor

---
+++ src/com/vengeful/sloths/AreaView/voCreationVisitor.java
@@ -0,0 +1,7 @@
+package com.vengeful.sloths.AreaView;
+
+/**
+ * Created by alexs on 2/23/2016.
+ */
+public class voCreationVisitor {
+}
---
+++ src/com/vengeful/sloths/GameLaunching/LevelFactory.java
@@ -0,0 +1,7 @@
+package com.vengeful.sloths.GameLaunching;
+
+/**
+ * Created by alexs on 2/23/2016.
+ */
+public class LevelFactory {
+}
