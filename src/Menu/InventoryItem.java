0c53313d3d849f9c34ce7e536597f524a8e2e9e4
JohnnyRacket <john.randolph.harrison@gmail.com>
2016-03-07 14:53:27 -0500

    added some menu stuff

---
+++ src/Menu/InventoryItem.java
@@ -0,0 +1,27 @@
+package Menu;
+
+import java.awt.*;
+
+/**
+ * Created by John on 2/18/2016.
+ */
+public class InventoryItem implements ScrollableMenuItem {
+
+    private String name;
+
+    public InventoryItem(String name){
+        this.name = name;
+    }
+
+    @Override
+    public void select(ScrollableMenuItemCommand command) {
+        //do something
+        command.execute();
+    }
+
+    @Override
+    public void paintComponent(Graphics2D g2d, int xPos, int yPos, int width, int height) {
+        g2d.setColor(Color.BLACK);
+        g2d.drawString(this.name, xPos, yPos);
+    }
+}
---
+++ src/Menu/ScrollableMenuItemCommand.java
@@ -0,0 +1,8 @@
+package Menu;
+
+/**
+ * Created by John on 2/19/2016.
+ */
+public interface ScrollableMenuItemCommand {
+    public void execute();
+}
