7a5003f558c8d390f2e593bb89d32cbd4b0a1467
Icavitt <icavitt18@gmail.com>
2016-03-08 13:21:32 -0500

    more loading done

--- src/com/vengeful/sloths/Models/SaveLoad/Loader.java
+++ src/com/vengeful/sloths/Models/SaveLoad/Loader.java
@@ -1,10 +1,11 @@
 package com.vengeful.sloths.Models.SaveLoad;
 
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.NodeType;
+import com.vengeful.sloths.Models.Entity.Avatar;
 import com.vengeful.sloths.Models.Map.MapArea;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import com.vengeful.sloths.Utility.Coord;
+import com.vengeful.sloths.Utility.Direction;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -33,17 +34,140 @@ public class Loader {
         doc = docBuilder.parse(fileName);
     }
 
-    public void loadAreas(String maName, MapArea[] areas){
+    public void loadAreas(MapArea[] areas){
         Node root = doc.getDocumentElement();
-        NodeList nl = doc.getChildNodes();
+        NodeList mapAreas = doc.getChildNodes();
         if(doc.hasChildNodes()){
-            for(int i = 0; i != nl.getLength(); ++i){
-                Node curr = nl.item(i);
-                if(curr.getNodeType() == Node.ELEMENT_NODE ){
-                    //unfinished processing
+            for(int i = 0; i != mapAreas.getLength(); ++i){
+                Node currMA = mapAreas.item(i);
+                if(currMA.getNodeType() == Node.ELEMENT_NODE && currMA.getNodeName().equals("MapArea")){
+                    if(currMA.hasChildNodes()){
+                        String maName = currMA.getAttributes().item(0).getNodeValue();
+                        MapArea loading;
+                        for(MapArea ma : areas){
+                            if(ma.getName().equals(maName)){
+                                loading = ma;
+                            }
+                        }
+                        NodeList tileObjects = currMA.getChildNodes();
+                        for(int j = 0; j != tileObjects.getLength(); ++j){
+                            Node currObject = tileObjects.item(j);
+                            if(currObject.getNodeType() != Node.ELEMENT_NODE){
+                                System.out.print("Expected an element node to be a child of map area");
+                                continue;
+                            }
+                            switch (currObject.getNodeName()){
+                                case "Avatar":
+                                    Avatar a = processAvatar(currObject);
+                                    break;
+
+                                default: System.out.println(currObject.getNodeName() + " doesn't have a case to handle it");
+                            }
+                        }
+
+                    }
+                }
+                else{
+                    System.out.println("MapArea expected got " + currMA.getNodeName() + " instead" );
                 }
             }
         }
+    }
+
+    private Avatar processAvatar(Node currObject) {
+        Avatar a = Avatar.getInstance();
+        NamedNodeMap avatarAttributes = currObject.getAttributes();
+        for(int i = 0; i != avatarAttributes.getLength(); ++i){
+            Node attribute = avatarAttributes.item(i);
+            String attributeName = attribute.getNodeName();
+            String value = attribute.getNodeValue();
+            switch (attributeName){
+                case "Direction":
+                    Direction dir = Direction.N;
+                    switch(value){
+                        case "N":
+                            dir = Direction.N;
+                            break;
+                        case "S":
+                            dir = Direction.S;
+                            break;
+                        case "NE":
+                            dir = Direction.NE;
+                            break;
+                        case "Nw":
+                            dir = Direction.NW;
+                            break;
+                        case "SE":
+                            dir = Direction.SE;
+                            break;
+                        case "SW":
+                            dir = Direction.SW;
+                            break;
+                        default:
+                            System.out.println(value + "is not a supporterd direction");
+                    }
+                    a.setFacingDirection(dir);
+                    break;
+                case "Name":
+                    a.setName(value);
+                    break;
+                default:
+                    System.out.println(attributeName + "is not a supported avatar attribute");
+            }
+        }
+        NodeList avatarObjects = currObject.getChildNodes();
+        for(int i = 0; i != avatarObjects.getLength(); ++i){
+            Node avatarObject = avatarObjects.item(i);
+            if(avatarObject != NodeType.Element){
+                System.out.println("Avatar object isn't an element type");
+                continue;
+            }
+            String objectName = avatarObject.getNodeName();
+            switch (objectName){
+                case "Location" :
+                    Coord c = processLocation(avatarObject);
+                    a.setLocation(c);
+                    break;
+                case "Sneak" :
+                    break;
+                case "Smasher" :
+                    break;
+                case "Summoner" :
+                    break;
+                case "Stats" :
+                    break;
+                case "AbilityManager" :
+                    break;
+                case "BuffManager" :
+                    break;
+                case "Inventory" :
+                    break;
+                case "Equipped" :
+                    break;
+                case "SkillManager" :
+                    break;
+                default:
+                    System.out.println(objectName + "is not a supported avatar object");
+            }
+        }
+        return a;
+    }
+
+    Coord processLocation(Node object){
+        int s;
+        int r;
+        Coord c = new Coord();
+        if(object == NodeType.Element){
+            Element locElement = (Element) object;
+            s = Integer.valueOf(locElement.getAttribute("s"));
+            r = Integer.valueOf(locElement.getAttribute("r"));
+            c.setR(r);
+            c.setS(s);
+        }
+        else{
+            System.out.println("node passed to process location wasn't an element");
+        }
+        return c;
     }
 
 }
