6a794a8980be1fed0bec42a87f8b21084fcaa17e
Ian Cavitt <icavitt18@gmail.com>
2016-03-12 15:52:54 -0500

    droping and losing gold on death

--- src/com/vengeful/sloths/Models/EntityMapInteractionCommands/DropSomeGoldCommand.java
+++ src/com/vengeful/sloths/Models/EntityMapInteractionCommands/DropSomeGoldCommand.java
@@ -1,34 +1,33 @@
 package com.vengeful.sloths.Models.EntityMapInteractionCommands;
 
-import com.vengeful.sloths.AreaView.TemporaryVOCreationVisitor;
 import com.vengeful.sloths.Models.Entity.Entity;
 import com.vengeful.sloths.Models.Inventory.Inventory;
 import com.vengeful.sloths.Models.Map.Map;
 import com.vengeful.sloths.Models.Map.MapItems.Gold;
-import com.vengeful.sloths.Models.Map.Tile;
 import com.vengeful.sloths.Utility.Coord;
 
 /**
  * Created by harrison on 3/12/16.
  */
-public class DropAllGoldCommand {
-    Entity entity;
-    Inventory inventory;
-    Coord dest;
+public class DropSomeGoldCommand {
+    private Entity entity;
+    private Inventory inventory;
+    private Coord dest;
+    private int value;
 
-    public DropAllGoldCommand(Entity entity, Coord dest){
-        this.entity = entity;
-        this.inventory = entity.getInventory();
-        this.dest = dest;
+    public DropSomeGoldCommand(Entity e, int value){
+        this.entity = e;
+        inventory = e.getInventory();
+        dest = e.getLocation();
+        this.value = value;
     }
 
     public void execute() {
         //inventory was null
         if(inventory.getGold() > 0){
-            Gold gold = inventory.dropAllGold();
+            Gold gold = inventory.dropSomeGold(value);
             gold.setLocation(dest);
             Map.getInstance().getActiveMapArea().getTile(dest).addGold(gold);
-            gold.accept(TemporaryVOCreationVisitor.getInstance());
         }
     }
 }
